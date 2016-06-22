cfadeefdfdd4ca8281f67d50ce8ede26578b4dad
David Begert <dbegert@usc.edu>
2016-06-22 14:40:22 -0700

    admins can edit, delete, and create users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+
   has_secure_password
 
   validates :email,
@@ -17,8 +18,6 @@ class User < ActiveRecord::Base
   def full_name
     "#{firstname} #{lastname}"
   end
-
-
 
 
 end
